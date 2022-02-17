bb383748e5539c283201996f8c829077252cc125
engsamar <eng.samar.2014@gmail.com>
2022-02-17 18:09:33 +0200

    upload initial file with articles package

---
+++ packages/ACME/Articles/src/Resources/lang/ar/app.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    'articles' => [
+        'title' => 'المقالات',
+        'save-btn-title'   => 'حفظ',
+        'add-title' => 'Add article',
+        'general' => 'General',
+        'name' => 'Name',
+        'position' => 'Position',
+        'seo'                      => 'Search Engine Optimization',
+        'slug'                     => 'Slug',
+        'meta_title'               => 'Meta Title',
+        'meta_description'         => 'Meta Description',
+        'meta_keywords'            => 'Meta Keywords',
+        'image'                    => 'Image',
+        'filterable-attributes'    => 'Filterable Attributes',
+        'attributes'               => 'Attributes',
+        'description'              => 'Description',
+        'description-and-images'   => 'Description and Images',
+        'add-image-btn-title'     => 'Add Image',
+         'edit-title'           => 'Edit Attribute',
+         'visible'  => 'Visible on Front-end',
+         'yes'                      => 'Yes',
+            'no'                       => 'No',
+    ]
+];
